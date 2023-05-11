fa4012d1b400250a8b66e5d325861a34d8f466c3
franco1988 <francoplazafc@gmail.com>
2023-05-11 13:36:26 -0300

    inicializando repo nextjs-pokemon-ts

--- pages/index.js
+++ pages/index.js
@@ -5,13 +5,13 @@ export default function Home() {
   return (
     <div className={styles.container}>
       <Head>
-        <title>Create Next App</title>
+        <title>Typescript</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+        Typescript
         </h1>
 
         <p className={styles.description}>
